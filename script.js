b4a5e76d179afe70a31bfb76dbe4fc6a328a1ab8
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2022-11-15 11:39:00 +0300

    fix linter errors

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-function show_menu() {
-  var menuBox = document.getElementById('menu-box');
-  if (menuBox.style.display == "block") { // if is menuBox displayed, hide it
-    menuBox.style.display = "none";
-  }
-  else { // if is menuBox hidden, display it
-    menuBox.style.display = "block";
+function showMenu() {
+  const menuBox = document.getElementById('menu-box');
+  if (menuBox.style.display === 'block') { // if is menuBox displayed, hide it
+    menuBox.style.display = 'none';
+  } else { // if is menuBox hidden, display it
+    menuBox.style.display = 'block';
   }
 }
 
-function close_menu() {
-  var menuBox = document.getElementById('menu-box');
-  menuBox.style.display = "none"
+showMenu();
+
+function closeMenu() {
+  const menuBox = document.getElementById('menu-box');
+  menuBox.style.display = 'none';
 }
+
+closeMenu();
