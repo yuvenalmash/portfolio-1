18b08f7d3cae422057e75fc37b888dd6dfdae425
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2022-11-16 01:46:25 +0300

    add show and close popup function

--- javaScript/script.js
+++ javaScript/script.js
@@ -7,11 +7,29 @@ function showMenu() {
   }
 }
 
-showMenu();
-
 function closeMenu() {
   const menuBox = document.getElementById('menu-box');
   menuBox.style.display = 'none';
 }
 
+// document.getElementById("myBtn").onclick = function() {showProject()};
+function showProject() {
+  const projectPopup = document.getElementById('popupBox')
+  projectPopup.style.display = 'block';
+  // const projectPopup = document.getElementById('popupBox');
+  // if (projectPopup.style.display === 'block') {
+  //   projectPopup.style.display = 'none';
+  // } else {
+  //   projectPopup.style.display = 'block';
+  // }
+}
+
+function closeProject() {
+  const projectPopup = document.getElementById('popupBox')
+  projectPopup.style.display = 'none';
+}
+
+showMenu();
 closeMenu();
+showProject();
+closeProject();
