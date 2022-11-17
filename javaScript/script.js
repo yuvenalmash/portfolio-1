a0c33b0cc7c1c1e3b2e5ec68c5e54198efa1d7f1
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2022-11-17 11:48:33 +0300

    fix linter errors

--- javaScript/script.js
+++ javaScript/script.js
@@ -8,58 +8,84 @@ function closeMenu() {
   menuBox.style.display = 'none';
 }
 
+function listProjects() {
+  const project1 = {
+    id: 1,
+    name: 'Multi-Post Stories',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+    featuredImageBig: 'images/desktop-size/ImgPlaceholder.png',
+    featuredImageSmall: 'images/featured-work/Img-Placeholder.png',
+    featuredImage: 'images/projects/project1/Snapshoot Portfolio(desktop).png',
+    technologies: ['css', 'html', 'bootstrap', 'Ruby'],
+    linkToLive: 'https://www.freecodecamp.org/news/what-is-a-fullstack-developer/',
+    linkToSource: 'https://github.com/yuvenalmash',
+  };
+  const project2 = {
+    id: 2,
+    name: 'REST API',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
+    featuredImage: 'images/projects/project1/Snapshoot Portfolio(desktop).png',
+    technologies: [],
+    linkToLive: 'https://www.freecodecamp.org/news/what-is-a-fullstack-developer/',
+    linkToSource: 'https://github.com/yuvenalmash',
+  };
+  const project3 = {
+    id: 3,
+    name: 'Data Dashboard Healthcare',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
+    featuredImage: 'images/projects/project1/Snapshoot Portfolio(desktop).png',
+    technologies: ['html', 'bootstrap', 'Ruby'],
+    linkToLive: 'https://www.freecodecamp.org/news/what-is-a-fullstack-developer/',
+    linkToSource: 'https://github.com/yuvenalmash',
+  };
+  const project4 = {
+    id: 4,
+    name: 'Website Portfolio',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
+    featuredImage: 'images/projects/project1/Snapshoot Portfolio(desktop).png',
+    technologies: ['html', 'bootstrap', 'Ruby'],
+    linkToLive: 'https://www.freecodecamp.org/news/what-is-a-fullstack-developer/',
+    linkToSource: 'https://github.com/yuvenalmash',
+  };
+  const project5 = {
+    id: 5,
+    name: 'Blog',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
+    featuredImage: 'images/projects/project1/Snapshoot Portfolio(desktop).png',
+    technologies: ['html', 'bootstrap', 'Ruby'],
+    linkToLive: 'https://www.freecodecamp.org/news/what-is-a-fullstack-developer/',
+    linkToSource: 'https://github.com/yuvenalmash',
+  };
+  const project6 = {
+    id: 6,
+    name: 'Data Dashboard Healthcare',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
+    featuredImage: 'images/projects/project1/Snapshoot Portfolio(desktop).png',
+    technologies: ['html', 'bootstrap', 'Ruby'],
+    linkToLive: 'https://www.freecodecamp.org/news/what-is-a-fullstack-developer/',
+    linkToSource: 'https://github.com/yuvenalmash',
+  };
+  const project7 = {
+    id: 7,
+    name: 'Website Portfolio',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
+    featuredImage: 'images/projects/project1/Snapshoot Portfolio(desktop).png',
+    technologies: ['html', 'bootstrap', 'Ruby'],
+    linkToLive: 'https://www.freecodecamp.org/news/what-is-a-fullstack-developer/',
+    linkToSource: 'https://github.com/yuvenalmash',
+  };
+  return [project1, project2, project3, project4, project5, project6, project7];
+}
+
 // WORKS POPUP SECTION
-function showProject(projectId) {
-  var projects = listProjects()
-  var projectIndex = projects.findIndex((object) => object.id == projectId);
-  var project = projects[projectIndex]
-  const projectPopup = document.getElementById('popupBox')
-  projectPopup.style.display = 'block';
-  createWorkPopup(project)
-}
 
-function closeProject() {
-  const projectPopup = document.getElementById('popupBox')
-  projectPopup.style.display = 'none';
-}
-
-function createFeaturedWorks(project, tagsListId) {
-  const featuredWorks = `
-        <div class="featured-content">
-          <div id=featured-image></div>
-          <div class="featured-textblock">
-            <h2 id="textblock-title">${project.name}</h2>
-            <p id="textblock-supporting-text">${project.description}</p>
-            <ul class="textblock-tags" id="${tagsListId}">
-            </ul>
-            <a class="textblock-link" id="textBlockLink" onclick="showProject(${project.id})" href="#">See Project</a>
-          </div>
-        </div>`
-
-  var image
-  if((window.matchMedia("(max-width: 767px)")).matches){
-    image = project.featuredImageSmall
-  }else {
-    image = project.featuredImageBig
+function generatelist(array, listId) {
+  const list = document.getElementById(listId);
+  for (let item = 0; item < array.length; item += 1) {
+    const li = document.createElement('li');
+    li.innerText = array[item];
+    list.appendChild(li);
   }
-
-  var parent = document.getElementById('worksContent')
-  parent.innerHTML += featuredWorks
-  generatelist(project.technologies, tagsListId)
-}
-
-function createOtherWorks(project, tagsListId) {
-  const otherWorks = `
-        <div class="content-defaults content2">
-          <h2 class="other-content-title">${project.name}</h2>
-          <p class="other-content-description">${project.description}</p>
-          <ul class="other-content-tags" id="${tagsListId}">
-          </ul>
-          <a class="other-content-link" id="otherContentLink" onclick="showProject(${project.id})" href="#">See Project</a>
-        </div>`
-  var parent = document.getElementById('otherContent')
-  parent.innerHTML += otherWorks
-  generatelist(project.technologies, tagsListId)
 }
 
 function createWorkPopup(project) {
@@ -86,103 +112,72 @@ function createWorkPopup(project) {
             </ul>
           </div>
         </div>
-      </div>`
-  var parent = document.getElementById('popupBox')
-  parent.innerHTML = popup
-  generatelist(project.technologies, "popupTags")
+      </div>`;
+  const parent = document.getElementById('popupBox');
+  parent.innerHTML = popup;
+  generatelist(project.technologies, 'popupTags');
 }
 
-function generatelist(array, listId) {
-  let list = document.getElementById(listId);
-  for (let item of array) {
-    let li = document.createElement("li");
-    li.innerText = item;
-    list.appendChild(li);
-  }
+function showProject(projectId) {
+  const projects = listProjects();
+  const projectIndex = projects.findIndex((object) => object.id === projectId);
+  const project = projects[projectIndex];
+  const projectPopup = document.getElementById('popupBox');
+  projectPopup.style.display = 'block';
+  createWorkPopup(project);
+}
+
+function closeProject() {
+  const projectPopup = document.getElementById('popupBox');
+  projectPopup.style.display = 'none';
+}
+
+function createFeaturedWorks(project, tagsListId) {
+  const featuredWorks = `
+        <div class="featured-content">
+          <div id=featured-image></div>
+          <div class="featured-textblock">
+            <h2 id="textblock-title">${project.name}</h2>
+            <p id="textblock-supporting-text">${project.description}</p>
+            <ul class="textblock-tags" id="${tagsListId}">
+            </ul>
+            <a class="textblock-link" id="textBlockLink" onclick="showProject(${project.id})" href="#">See Project</a>
+          </div>
+        </div>`;
+
+  const parent = document.getElementById('worksContent');
+  parent.innerHTML += featuredWorks;
+  generatelist(project.technologies, tagsListId);
+}
+
+function createOtherWorks(project, tagsListId) {
+  const otherWorks = `
+        <div class="content-defaults content2">
+          <h2 class="other-content-title">${project.name}</h2>
+          <p class="other-content-description">${project.description}</p>
+          <ul class="other-content-tags" id="${tagsListId}">
+          </ul>
+          <a class="other-content-link" id="otherContentLink" onclick="showProject(${project.id})" href="#">See Project</a>
+        </div>`;
+  const parent = document.getElementById('otherContent');
+  parent.innerHTML += otherWorks;
+  generatelist(project.technologies, tagsListId);
 }
 
 function pageLoad() {
-  var projects = listProjects()
-  for (let x = 0; x < projects.length; x++) {
-    var tagsListId = "tagsListId" + (x.toString())
+  const projects = listProjects();
+  for (let x = 0; x < projects.length; x += 1) {
+    const tagsListId = `tagsListId${x.toString()}`;
     if (x === 0) {
-      createFeaturedWorks(projects[x], tagsListId)
+      createFeaturedWorks(projects[x], tagsListId);
     } else {
-      createOtherWorks(projects[x], tagsListId)
+      createOtherWorks(projects[x], tagsListId);
     }
   }
-}
-
-function listProjects() {
-  var project1 = {
-    id: 1,
-    name: 'Multi-Post Stories',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
-    featuredImageBig: 'images/desktop-size/ImgPlaceholder.png',
-    featuredImageSmall: 'images/featured-work/Img-Placeholder.png',
-    featuredImage: 'images/projects/project1/Snapshoot Portfolio(desktop).png',
-    technologies: ['css', 'html', 'bootstrap', 'Ruby'],
-    linkToLive: 'https://www.freecodecamp.org/news/what-is-a-fullstack-developer/',
-    linkToSource: 'https://github.com/yuvenalmash',
-  };
-  var project2 = {
-    id: 2,
-    name: 'REST API',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
-    featuredImage: 'images/projects/project1/Snapshoot Portfolio(desktop).png',
-    technologies: [],
-    linkToLive: 'https://www.freecodecamp.org/news/what-is-a-fullstack-developer/',
-    linkToSource: 'https://github.com/yuvenalmash',
-  };
-  var project3 = {
-    id: 3,
-    name: 'Data Dashboard Healthcare',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
-    featuredImage: 'images/projects/project1/Snapshoot Portfolio(desktop).png',
-    technologies: ['html', 'bootstrap', 'Ruby'],
-    linkToLive: 'https://www.freecodecamp.org/news/what-is-a-fullstack-developer/',
-    linkToSource: 'https://github.com/yuvenalmash'
-  };
-  var project4 = {
-    id: 4,
-    name: 'Website Portfolio',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
-    featuredImage: 'images/projects/project1/Snapshoot Portfolio(desktop).png',
-    technologies: ['html', 'bootstrap', 'Ruby'],
-    linkToLive: 'https://www.freecodecamp.org/news/what-is-a-fullstack-developer/',
-    linkToSource: 'https://github.com/yuvenalmash'
-  };
-  var project5 = {
-    id: 5,
-    name: 'Blog',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
-    featuredImage: 'images/projects/project1/Snapshoot Portfolio(desktop).png',
-    technologies: ['html', 'bootstrap', 'Ruby'],
-    linkToLive: 'https://www.freecodecamp.org/news/what-is-a-fullstack-developer/',
-    linkToSource: 'https://github.com/yuvenalmash'
-  };
-  var project6 = {
-    id: 6,
-    name: 'Data Dashboard Healthcare',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
-    featuredImage: 'images/projects/project1/Snapshoot Portfolio(desktop).png',
-    technologies: ['html', 'bootstrap', 'Ruby'],
-    linkToLive: 'https://www.freecodecamp.org/news/what-is-a-fullstack-developer/',
-    linkToSource: 'https://github.com/yuvenalmash'
-  };
-  var project7 = {
-    id: 7,
-    name: 'Website Portfolio',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
-    featuredImage: 'images/projects/project1/Snapshoot Portfolio(desktop).png',
-    technologies: ['html', 'bootstrap', 'Ruby'],
-    linkToLive: 'https://www.freecodecamp.org/news/what-is-a-fullstack-developer/',
-    linkToSource: 'https://github.com/yuvenalmash'
-  };
-  return [project1, project2, project3, project4, project5, project6, project7]
 }
 
 showMenu();
 closeMenu();
 showProject();
 closeProject();
+pageLoad();
